c7c118dc29c81da10a159f56bff7741e80fb41e8
Bradley-Bird <Bradleybird2@icloud.com>
2022-10-05 19:44:14 -0700

    removed unused props

--- src/Views/Main.tsx
+++ src/Views/Main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import BmiForm from '../Components/BmiForm';
 
 function Main() {
-    return <BmiForm height1={1} weight1={2} />;
+    return <BmiForm />;
 }
 
 export default Main;
